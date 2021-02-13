dd2ac2b09e68327bb856701163b5761e8d959593
yongyong-0 <comkorea123@gmail.com>
2021-02-14 01:01:31 +0900

    Add basic structure
    delete default components
    To do : content layout

---
+++ frontend/src/components/Content.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="content">
+		<h2>{{ data }}</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			data: Object,
+		},
+
+	}
+</script>
+
+<style scoped>
+.content {
+	/* width: 100%;
+	height: 100%; */
+	/* min-width: 500px;
+	min-height: 500px; */
+	
+	border: dashed 4px lightsalmon;
+}
+</style>
---
+++ frontend/src/components/Contents.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="contents">
+    Contents<br>
+    commit wave(grass) present area
+    <div 
+    class="content"
+    v-for="dummy in dummyData" 
+    :key="dummy.id">
+      <Content @data="dummy"/>
+    </div>
+    {{dummyData}}
+  </div>
+</template>
+
+<script>
+import Content from "@/components/Content"
+
+const dummies = Array();
+for(let i=0; i<10; i++) {
+  dummies.push({title: "Team" + i})
+}
+export default {
+  data() {
+    return {
+      dummyData: dummies 
+    }
+  },
+  components: {
+    Content
+  }
+}
+</script>
+
+<style scoped>
+.contents {
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  min-width: 500px;
+  min-height: 500px;
+  
+  border: dashed 4px lightgreen;
+}
+
+.content {
+  width: 50%;
+  height: 200px;
+}
+</style>
