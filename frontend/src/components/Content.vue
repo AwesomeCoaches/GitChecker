127e19c03518adc89a36fd79ff05f6bdb45d2c7a
정용우 <comkorea123@gmail.com>
2021-02-15 09:26:42 +0900

    Add basic structure

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -1,25 +1,30 @@
 <template>
-	<div class="content">
-		<h2>{{ data }}</h2>
-	</div>
+  <div class="content">
+    Content<br />
+    {{ dummy }}
+  </div>
 </template>
 
 <script>
-	export default {
-		props: {
-			data: Object,
-		},
-
-	}
+export default {
+  mounted() {
+    console.log(this.dummy);
+  },
+  props: {
+    dummy: Object,
+  },
+};
 </script>
 
 <style scoped>
 .content {
-	/* width: 100%;
+  /* width: 100%;
 	height: 100%; */
-	/* min-width: 500px;
+  /* min-width: 500px;
 	min-height: 500px; */
-	
-	border: dashed 4px lightsalmon;
+
+  height: 200px;
+  border: dashed 4px lightsalmon;
+  background: lightyellow;
 }
 </style>
--- frontend/src/components/Contents.vue
+++ frontend/src/components/Contents.vue
@@ -1,49 +1,46 @@
 <template>
   <div class="contents">
-    Contents<br>
-    commit wave(grass) present area
-    <div 
-    class="content"
-    v-for="dummy in dummyData" 
-    :key="dummy.id">
-      <Content @data="dummy"/>
+    Contents<br />
+    commit wave(grass) present area<br /><br />
+    Dummies is : {{ dummyData }}
+    <div v-for="dummy in dummyData" :key="dummy.id">
+      <Content :dummy="dummy" />
     </div>
-    {{dummyData}}
   </div>
 </template>
 
 <script>
-import Content from "@/components/Content"
+import Content from "@/components/Content";
 
 const dummies = Array();
-for(let i=0; i<10; i++) {
-  dummies.push({title: "Team" + i})
+for (let i = 0; i < 10; i++) {
+  dummies.push({ title: "Team" + i });
 }
 export default {
   data() {
     return {
-      dummyData: dummies 
-    }
+      dummyData: dummies,
+    };
   },
   components: {
-    Content
-  }
-}
+    Content,
+  },
+};
 </script>
 
 <style scoped>
 .contents {
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
   width: 80vw;
   min-width: 500px;
   min-height: 500px;
-  
+
   border: dashed 4px lightgreen;
 }
 
-.content {
-  width: 50%;
-  height: 200px;
+.contents > div {
+  flex: 1 40%;
+  margin: 10px;
 }
 </style>
