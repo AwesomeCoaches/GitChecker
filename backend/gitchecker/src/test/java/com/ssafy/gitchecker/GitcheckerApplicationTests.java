c36500fbc2a90eb54976bcc014cfb62df7569f52
coach42 <ssafy_coach_42@ssafy.com>
2021-02-16 17:32:37 +0900

    Initial Spring

---
+++ backend/gitchecker/src/test/java/com/ssafy/gitchecker/GitcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.gitchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
