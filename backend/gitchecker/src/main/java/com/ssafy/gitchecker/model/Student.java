df699680d59fe3d688a8ff3a2fb0661c8ef4bfd7
tlsdorye <tlsdorye@gmail.com>
2021-02-18 16:45:44 +0900

    update model

---
+++ backend/gitchecker/src/main/java/com/ssafy/gitchecker/model/Student.java
@@ -0,0 +1,59 @@
+package com.ssafy.gitchecker.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+
+import com.ssafy.gitchecker.payload.StudentResponse;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    private String name;
+
+    @Column(nullable = false, updatable = false, unique = true)
+    private String gitId;
+
+    @Email
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, updatable = false)
+    private int grp;
+
+    @Column(nullable = false, updatable = false)
+    private String city;
+
+    @Column(nullable = false, updatable = false)
+    private String cls;
+
+    @Column(nullable = false)
+    private String teamId;
+
+    private String imageUrl;
+
+    public StudentResponse toResponse() {
+        return StudentResponse.builder()
+                .id(id)
+                .name(name)
+                .gitId(gitId)
+                .email(email)
+                .grp(grp)
+                .city(city)
+                .cls(cls)
+                .teamId(teamId)
+                .imageUrl(imageUrl)
+                .build();
+    }
+}
