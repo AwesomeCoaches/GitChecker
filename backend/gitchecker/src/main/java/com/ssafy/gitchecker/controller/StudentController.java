43bfe7dd29c8bdc16c5115fdfe0f625949e1a2f1
coach42 <ssafy_coach_42@ssafy.com>
2021-02-19 16:03:01 +0900

    Use Api to get all projects

--- backend/gitchecker/src/main/java/com/ssafy/gitchecker/controller/StudentController.java
+++ backend/gitchecker/src/main/java/com/ssafy/gitchecker/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.ssafy.gitchecker.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -7,14 +10,24 @@ import com.ssafy.gitchecker.exception.ResourceNotFoundException;
 import com.ssafy.gitchecker.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
+
+    private static final String BaseUrl = "https://lab.ssafy.com/api/v4";
+    private static final String privateToken = "a3sroJgsz_iuJxeT9-Mj";
 
     @Autowired
     private StudentRepository sr;
@@ -35,4 +48,27 @@ public class StudentController {
         return sr.findAll().stream().map(s -> s.toResponse());
     }
 
+    @GetMapping("/getInfo")
+    public Object getInfo(@RequestParam(required = false, name = "search") @Size(min = 1, max = 10) String search) {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(3000);
+
+        RestTemplate rt = new RestTemplate(factory);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("PRIVATE-TOKEN", privateToken);
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(BaseUrl + "/projects")
+        .queryParam("simple", "true");
+        if(search != null) builder.queryParam("search", search);
+
+        HttpEntity<Map<String,String>> httpEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> responseEntity = rt.exchange(builder.toUriString(), HttpMethod.GET, httpEntity, String.class);
+
+        // System.out.println(responseEntity.getBody());
+
+        return responseEntity.getBody();
+    }
 }
