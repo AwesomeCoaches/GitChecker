b210dc68bcba2b3da3ebd7fd55ecd037b3e9f6e5
yongyong-0 <comkorea123@gmail.com>
2021-03-02 00:42:41 +0900

    Add sidebar filter, chart demo

--- frontend/src/components/Contents.vue
+++ frontend/src/components/Contents.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="contents">
-    Contents<br />
+    <!-- Contents<br />
     commit wave(grass) present area<br /><br />
-    Dummies is : {{ dummyData }}
-    <div v-for="dummy in dummyData" :key="dummy.id">
+    Dummies is : {{ dummyData }} -->
+    <!-- <div v-for="dummy in dummyData" :key="dummy.id">
       <Content :dummy="dummy" />
+    </div> -->
+    <div class="charts">
+      <AreaChart/>
+      <PieChart/>
+      <BarChart/>
     </div>
   </div>
 </template>
 
 <script>
-import Content from "@/components/Content";
+// import Content from "@/components/Content";
+import AreaChart from "@/components/chart/AreaChart.vue";
+import PieChart from "@/components/chart/PieChart.vue";
+import BarChart from "@/components/chart/BarChart.vue";
 
 const dummies = Array();
 for (let i = 0; i < 10; i++) {
@@ -20,15 +28,23 @@ export default {
   data() {
     return {
       dummyData: dummies,
+      
     };
   },
   components: {
-    Content,
+    // Contents,
+    AreaChart,
+    PieChart,
+    BarChart,
   },
 };
 </script>
 
 <style scoped>
+.charts {
+  display: flex;
+  height: 300px;
+}
 .contents {
   display: flex;
   flex-wrap: wrap;
--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,22 +1,65 @@
 <template>
-    <div class=sidebar>
-        Sidebar<br>
-        filter will present here
+  <div class=sidebar>
+    <div class="filter">
+      <div>지역</div>
+      <div v-for="item in city" :key="item.id">
+        <!-- <router-link :to="`city=${item}`"> {{item}} </router-link> -->
+        <input type="checkbox" v-model="checkbox.city" :value="item">
+        <label> {{ item }}</label>
+      </div>
     </div>
+    <div class="filter">
+      <div>반</div>
+      <div v-for="item in classes" :key="item.id">
+        <!-- <router-link :to="`class=${item}`"> {{item}} </router-link> -->
+        <input type="checkbox" v-model="checkbox.classes" :value="item">
+        <label> {{ item }}</label>
+      </div>
+    </div>
+    <div class="filter">
+      <div>팀</div>
+      <div v-for="item in team" :key="item.id">
+        <!-- <router-link :to="`team=${item}`"> {{item}} </router-link> -->
+        <input type="checkbox" v-model="checkbox.team" :value="item">
+        <label> {{ item }}</label>
+      </div>
+    </div>
+    <v-btn @click="applyFilter"> 적용 </v-btn>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+  export default {
+    data() {
+      return {
+        city: ["서울", "대전", "광주", "구미"],
+        classes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        team: [1, 2, 3, 4, 5, 6, 7, 8],
+        checkbox: {
+          city: [],
+          classes: [],
+          team: [],
+        }
+      }
+    },
+    methods: {
+      applyFilter() {
+        console.log(this.checkbox); 
+      }
+    },
+  }
 </script>
 
 <style scoped>
 .sidebar {
-    width: 20vw;
-    min-width: 200px;
-    min-height: 500px;
-    
-    border: dashed 4px skyblue;
+  width: 250px;
+  min-width: 200px;
+  min-height: 500px;
+  padding: 12px;
+  border: dashed 4px skyblue;
+}
+
+.filter {
+  margin: 20px 0;
 }
 </style>
