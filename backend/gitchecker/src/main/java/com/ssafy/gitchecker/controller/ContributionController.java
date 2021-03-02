207f59b2be9e41f396c5c71b41be23974027a8cf
tlsdorye <tlsdorye@gmail.com>
2021-03-02 13:32:54 +0900

    Add contribution create API

--- backend/gitchecker/src/main/java/com/ssafy/gitchecker/controller/ContributionController.java
+++ backend/gitchecker/src/main/java/com/ssafy/gitchecker/controller/ContributionController.java
@@ -3,10 +3,16 @@ package com.ssafy.gitchecker.controller;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.ssafy.gitchecker.exception.ResourceNotFoundException;
+import com.ssafy.gitchecker.model.Contribution;
+import com.ssafy.gitchecker.payload.AddContributionRequest;
 import com.ssafy.gitchecker.repository.ContributionRepository;
+import com.ssafy.gitchecker.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/contributions")
 public class ContributionController {
     
-    @Autowired ContributionRepository cr;
+    @Autowired 
+    ContributionRepository cr;
+
+    @Autowired
+    StudentRepository sr;
 
     @GetMapping("/")
     public Object getContribution(
@@ -30,6 +40,17 @@ public class ContributionController {
         if(teamId != null) return cr.findByStudent_TeamId(teamId).stream().map(c -> c.toResponse());
         if(username != null) return cr.findByStudent_Username(username).stream().map(c -> c.toResponse());
         return cr.findAll().stream().map(c -> c.toResponse());
+    }
+
+    @PostMapping("/")
+    public Object addContribution(@RequestBody AddContributionRequest request) {
+        return cr.save(new Contribution(
+            null,
+            sr.findByUsername(request.getUsername())
+                .orElseThrow(() -> new ResourceNotFoundException("student", "username", request.getUsername())),
+            request.getCount(),
+            request.getDate()
+        ));
     }
 
 }
