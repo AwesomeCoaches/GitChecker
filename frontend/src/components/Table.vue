ba0f5c6c6259ab4fd81089837b9643c60a7d6ab8
실습코치39 <ssafy_coach_39@ssafy.com>
2021-03-03 15:59:37 +0900

    Table modifying

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,23 +1,38 @@
 <template>
   <div style="width:100%; height:100%;">
-     <v-simple-table
+    <div class="dayRect" style="position:absolute; background-color:white; box-shadow:1px 1px 1px black;" :style="{display:thisDay.ondisplay,'margin-left':thisDay.marginLeft+'px','margin-top':thisDay.marginTop+'px'}">{{thisDay.day}}</div>
+    <v-simple-table
     fixed-header
-    height="100%"
-  >
-    <template v-slot:default>
-      <thead>
+    padding="0"
+    height="80vh"
+    width="90vw"
+    >
+    <template v-slot:default >
+      <thead >
         <tr>
-          <th>
+          <th rowspan="2" style="width:5%;">
             이름
           </th>
-          <th>
+           <th rowspan="2" style="width:4%;">
+            지역/반
+          </th>
+          <th rowspan="2" style="width:7%;">
             지라코드
           </th>
-          <th v-for="week in 6" :key="week">
-            <tr>{{week}}주차</tr>
-            <tr>
-              <th v-for="day in 5" :key="day">{{day}}</th>
-            </tr>
+          <th style="width:14%;" v-for="week in 6" :key="week">
+            <td colspan="5" style="width:100%;">{{week}}주차</td>
+          </th>
+        </tr>
+        <tr>
+          <th style="width:13%;" v-for="week in 6" :key="week">
+              <div class="weekday cell" @mouseover="mouseoverDay($event,week*7-7)" @mouseout="mouseoutDay">월</div>
+              <div class="weekday cell" @mouseover="mouseoverDay($event,week*7-6)" @mouseout="mouseoutDay">화</div>
+              <div class="weekday cell" @mouseover="mouseoverDay($event,week*7-5)" @mouseout="mouseoutDay">수</div>
+              <div class="weekday cell" @mouseover="mouseoverDay($event,week*7-4)" @mouseout="mouseoutDay">목</div>
+              <div class="weekday cell" @mouseover="mouseoverDay($event,week*7-3)" @mouseout="mouseoutDay">금</div>
+              <div class="weekend cell" @mouseover="mouseoverDay($event,week*7-2)" @mouseout="mouseoutDay">토</div>
+              <div class="weekend cell" @mouseover="mouseoverDay($event,week*7-1)" @mouseout="mouseoutDay">일</div>
+              <div class="cell">C</div>
           </th>
         </tr>
       </thead>
@@ -26,7 +41,13 @@
           v-for="(student,index) in students"
           :key="(student,index)"
         >
-          <td>{{ student.name }}</td>
+          <td class="fix">{{student.name}}</td>
+          <td class="fix">{{student.city}}/{{student.class}}</td>
+          <td class="fix">s04p13{{student.team_id}}</td>
+          <td v-for="week in 6" :key="week">
+            <div v-for="day in 7" :key="day" class="cell" :style="{'box-shadow':rows[(week-1)*5+(day-1)]}" @mouseover="mouseoverDay($event,(week-1)*5+(day))" @mouseout="mouseoutDay">0</div>
+          <div class="cell">X</div>
+          </td>
         </tr>
       </tbody>
     </template>
@@ -36,6 +57,8 @@
 
 <script>
 import axios from 'axios';
+// const URL = 'http://t4coach44.p.ssafy.io/api';
+const URL = 'http://localhost:8080';
 export default {
     components: {
     
@@ -44,80 +67,42 @@ export default {
       return {
         studentNum: 0,
         students: [],
-        loading: false,
-        options: {},
-        headers: [
-
-          { text: '지역', value: 'city' },
-          { text: '반', value: 'class' },
-          { text: '팀', value: 'team_id' },
-          { text: '이름', value: 'name' }
-        ],
-        getitems:''
+        contributions:[],
+        projectTerms:[],
+        getitems:'',
+        nowProjectType:2, //현재 진행하고 있는 프로젝트 타입
+        days:[],
+        Today:'',
+        thisDay:'',
+        rows:[],
+        commitDatas:[]
       }
   },
     mounted(){
-      // setTimeout(this.getStudents (), 1000)
+      this.getContributions()
         this.getStudents ()
-        // console.log(this.getitems)
-        // this.getDataFromApi();
-
+        this.getProjectTerms()
+        setTimeout(() => {
+         this.setContributions()
+        }, 1000);
     },
     methods:{
-      getDataFromApi () {
-        this.loading = true
-        this.fakeApiCall().then(data => {
-          console.log(data);
-          this.desserts = data.items
-          this.totalDesserts = data.total
-          this.loading = false
-        })
-      },
+      mouseoverDay(event,day){
+      var clientRect = event.target.getBoundingClientRect();
+      var dayRectMarginLeft = clientRect.left -300;
+      var dayRectMarginTop= clientRect.top-100;
+      this.thisDay = {day : this.days[day], ondisplay:'block', marginLeft:dayRectMarginLeft, marginTop: dayRectMarginTop};
+    },
+    mouseoutDay(){
+      this.thisDay = {day : '', ondisplay:'none'}
+    },
       /**
        * In a real application this would be a call to fetch() or axios.get()
        */
-      fakeApiCall () {
-        return new Promise((resolve) => {
-          const { sortBy, sortDesc, page, itemsPerPage } = this.options
-
-          // this.getStudents()
-          let items = this.getitems    
-          console.log(items)
-          const total = this.items.length
-
-          if (sortBy.length === 1 && sortDesc.length === 1) {
-            items = items.sort((a, b) => {
-              const sortA = a[sortBy[0]]
-              const sortB = b[sortBy[0]]
-
-              if (sortDesc[0]) {
-                if (sortA < sortB) return 1
-                if (sortA > sortB) return -1
-                return 0
-              } else {
-                if (sortA < sortB) return -1
-                if (sortA > sortB) return 1
-                return 0
-              }
-            })
-          }
-
-          if (itemsPerPage > 0) {
-            items = items.slice((page - 1) * itemsPerPage, page * itemsPerPage)
-          }
-
-          setTimeout(() => {
-            resolve({
-              items,
-              total,
-            })
-          }, 1000)
-        })
-      },
       getStudents () {
-        axios.get(`http://localhost:8080/students/`)
+        axios.get(`${URL}/students/`)
         .then((res)=>{
-          console.log(res);
+          // console.log(res);
           this.students = res.data;
           this.totalDesserts = res.data.length;
         })
@@ -127,18 +112,83 @@ export default {
         )
       },
       getContributions () {
-        axios.get(`http://localhost:8080/contributions/`)
+        axios.get(`${URL}/contributions/`)
         .then((res)=>{
           console.log(res);
-          this.students = res.data;
-          this.totalDesserts = res.data.length;
+          this.contributions = res.data;
         })
         .catch(function (error) {
             console.log(error)
         }
         )
       },
-    }
+    setContributions(){
+      this.contributions.forEach(contribution =>{
+          // console.log(contribution);
+          var conDay = contribution.date.split("T")[0];
+          var computingDayNum = 0;
+          for(var i=0; i<42;i++){
+            if(conDay==this.days[i]){
+              computingDayNum = i;
+              console.log(this.days[i]);
+          }
+          var commitColor = '';
+          if (contribution.cnt>40) {
+            commitColor = "green";
+          }else{
+            commitColor = "yellow"
+          }
+          this.commitDatas[contribution.student.id*100+computingDayNum] = {num:contribution.cnt,color:commitColor}
+        } 
+      });//end of forEach
+    console.log(this.commitDatas);
+    },
+    getProjectTerms () {
+        // axios.get(`http://localhost:8080/contributions/`)
+        // .then((res)=>{
+        //   console.log(res);
+        //   this.students = res.data;
+        //   this.totalDesserts = res.data.length;
+        // })
+        // .catch(function (error) {
+        //     console.log(error)
+        // }
+        // )
+        this.projectTerms = [
+         {
+           id:1, end_date:'2021-04-09',grp:1,start_date:'2021-03-01',type:2
+         }, 
+         
+        ]
+        
+        var moment = require('moment');
+        var day = moment(this.projectTerms[0].start_date)
+        var today = moment().format('YYYY-MM-DD');
+        // // for(var w=0; w<6; w++){
+        // //   for(var i=0; i<5; i++){
+        // //     // console.log(day)
+        // //     this.days[w*5+i] =  day.format('YYYY-MM-DD');
+        // //     day.add(1,'days');
+        // //     if(day.format('YYYY-MM-DD') == today){
+        // //       this.Today = w*5+i+1;
+        // //       this.rows[this.Today] = '1px 1px 6px #0000004d';
+        // //       console.log(day.format('YYYY-MM-DD'))
+        // //     }
+        // //   }
+        // //       day.add(2,'days')
+        // // }
+        for(var w=0;w<42;w++){
+          this.days[w] =  day.format('YYYY-MM-DD');
+          day.add(1,'days');
+          if(day.format('YYYY-MM-DD') == today){
+            this.Today = w+1;
+            this.rows[this.Today] = '1px 1px 6px #0000004d';
+          }
+        }
+
+      // console.log(this.rows);
+      },
+      },
 }
 </script>
 
@@ -156,7 +206,36 @@ export default {
 }
 .container--fluid {
     max-width: 100%;
-    overflow-y: scroll;
-    height: 100%;
+    /* overflow-y: scroll; */
+    /* height: 100%; */
+}
+
+.cell{
+  width: 12.5%;
+  float: left;
+  text-align: center;
+  cursor: pointer
+}
+tr{
+  width:100%
+}
+td,th{
+  padding: 0 0 !important;
+  border: 1px solid #00000005;
+}
+
+th>td{
+  padding: 0 0 !important;
+  border: none;
+}
+
+.fix{
+  position: inherit;
+}
+.weekday{
+  color:black;
+}
+.weekend{
+  color:red;
 }
 </style>
